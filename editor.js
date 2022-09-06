59196f15c80bdd2f84d5aee7c9ee0b5b70058d0c
bubliman <ogabrhelik@ahojky.com>
2022-09-06 20:59:53 +0200

    first version (not working)

---
+++ editor.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('button').addEventListener('click', console.log('hello world'))
+
+// function getTask() {
+//     var task = document.getElementById('task-input')
+//     console.log(task)
+// }
+// function createTask(task) {
+//     document.getElementById("task1").innerHTML = task;
+// }
