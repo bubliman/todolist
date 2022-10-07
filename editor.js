063ea896a0737a8b3d01eb19732f547e24b99d37
bubliman <ogabrhelik@ahojky.com>
2022-10-07 22:40:26 +0200

    bugfixes

--- editor.js
+++ editor.js
@@ -1,67 +1,67 @@
  class Model {
     static idCountTask = 0
     constructor() {
-      // this.list = JSON.parse(localStorage.getItem('list')) || []
-      this.list = new Array()
+      this.list = JSON.parse(localStorage.getItem('list')) || []
+      // this.list = new Array()
       this.list.forEach((element) => {
         if (element.id > Model.idCountTask) {
           Model.idCountTask = element.id
         }
       })
-      this.list[0] = {
-        id: ++Model.idCountTask,
-        text: 'finised task example 1',
-        status: ModelTask.statusFinish,
-        schedule: false,
-      }
-      this.list[1] = {
-        id: ++Model.idCountTask,
-        text: 'finised task example 2',
-        status: ModelTask.statusFinish,
-        schedule: false,
-      }
-      this.list[2] = {
-        id: ++Model.idCountTask,
-        text: 'finised task example 3',
-        status: ModelTask.statusFinish,
-        schedule: false,
-      }
-      this.list[3] = {
-        id: ++Model.idCountTask,
-        text: 'task example 1',
-        status: ModelTask.statusNone,
-        schedule: false,
-      }
-      this.list[4] = {
-        id: ++Model.idCountTask,
-        text: 'task example 2',
-        status: ModelTask.statusNone,
-        schedule: false,
-      }
-      this.list[5] = {
-        id: ++Model.idCountTask,
-        text: 'task example 3',
-        status: ModelTask.statusNone,
-        schedule: false,
-      }
-      this.list[6] = {
-        id: ++Model.idCountTask,
-        text: 'removed task example 1',
-        status: ModelTask.statusRemove,
-        schedule: false,
-      }
-      this.list[7] = {
-        id: ++Model.idCountTask,
-        text: 'removed task example 2',
-        status: ModelTask.statusRemove,
-        schedule: false,
-      }
-      this.list[8] = {
-        id: ++Model.idCountTask,
-        text: 'removed task example 3',
-        status: ModelTask.statusRemove,
-        schedule: false,
-      }
+    //   this.list[0] = {
+    //     id: ++Model.idCountTask,
+    //     text: 'finished task example 1',
+    //     status: ModelTask.statusFinish,
+    //     schedule: false,
+    //   }
+    //   this.list[1] = {
+    //     id: ++Model.idCountTask,
+    //     text: 'finished task example 2',
+    //     status: ModelTask.statusFinish,
+    //     schedule: false,
+    //   }
+    //   this.list[2] = {
+    //     id: ++Model.idCountTask,
+    //     text: 'finished task example 3',
+    //     status: ModelTask.statusFinish,
+    //     schedule: false,
+    //   }
+    //   this.list[3] = {
+    //     id: ++Model.idCountTask,
+    //     text: 'task example 1',
+    //     status: ModelTask.statusNone,
+    //     schedule: false,
+    //   }
+    //   this.list[4] = {
+    //     id: ++Model.idCountTask,
+    //     text: 'task example 2',
+    //     status: ModelTask.statusNone,
+    //     schedule: false,
+    //   }
+    //   this.list[5] = {
+    //     id: ++Model.idCountTask,
+    //     text: 'task example 3',
+    //     status: ModelTask.statusNone,
+    //     schedule: false,
+    //   }
+    //   this.list[6] = {
+    //     id: ++Model.idCountTask,
+    //     text: 'removed task example 1',
+    //     status: ModelTask.statusRemove,
+    //     schedule: false,
+    //   }
+    //   this.list[7] = {
+    //     id: ++Model.idCountTask,
+    //     text: 'removed task example 2',
+    //     status: ModelTask.statusRemove,
+    //     schedule: false,
+    //   }
+    //   this.list[8] = {
+    //     id: ++Model.idCountTask,
+    //     text: 'removed task example 3',
+    //     status: ModelTask.statusRemove,
+    //     schedule: false,
+    //   }
     }
   
     bindTaskListChanged(callback) {
@@ -100,6 +100,13 @@
   
       this._commit(this.list)
     }
+
+    recoverTask(id) {
+      let task = this.list.find(element => element.id === id)
+      task.recoverModelTask(id)
+  
+      this._commit(this.list)
+    }  
   
     finishTask(id) {
       let task = this.list.find(element => element.id === id)
@@ -144,7 +151,11 @@
     removeModelTask(id) {
       this.id === id ? this.status = ModelTask.statusRemove : this.status
     }
-  
+    
+    recoverModelTask(id) {
+      this.id === id ? this.status = ModelTask.statusNone : this.status
+    }
+
     finishModelTask(id) {
       this.id === id ? this.status = ModelTask.statusFinish : this.status
     }
@@ -159,36 +170,61 @@
   class View {
     // construct the initial View
     constructor() {
+
       // find a place to render our application
       this.app = this.getElement('#root')
+
       // asign the creation of certain HTML element to class attributes
+
         // create a form
         this.form = this.createElement('form')
+        this.form.classList.add('task-list')
+
         // create input for entering new tasks inside of the form
         this.input = this.createElement('input')
           // add atributes to the input
           this.input.type = 'text'
           this.input.placeholder = 'Add task'
           this.input.name = 'task'
+
         // create a submit button
         this.submitButton = this.createElement('button')
           // add atributes to the submit button
           this.submitButton.textContent = 'Submit'
-        // create a title of the list
-        this.title = this.createElement('h1')
-          // add atributes to the submit button
-          this.title.textContent = 'Tasks'
+
         // create u-lists
         this.taskList = this.createElement('ul', 'task-list')
         this.finishList = this.createElement('ul', 'finish-list')
         this.removeList = this.createElement('ul', 'remove-list')
           // add attributes to the bottom lists
-          this.finishList.classList.add('bottom-list')
-          this.removeList.classList.add('bottom-list')
+          // this.finishList.classList.add('bottom-list')
+          // this.removeList.classList.add('bottom-list')
+          // add titles to the lists
+            // create a h1 title 
+            this.h1 = this.createElement('h1')
+            // add atributes to the title
+            this.h1.textContent = 'Tasks'
+            // create a h4 title
+            const h4 = this.createElement('h4')
+            // add atributes to the finish-title
+            h4.textContent = 'Finished tasks'
+            this.h4finish = h4.textContent
+            // add atributes to the remove-title
+            h4.textContent = 'Removed tasks'
+            this.h4remove = h4.textContent 
+
       // append the task input and submit button to the form
       this.form.append(this.input, this.submitButton)
+
+      // append the h4 titles to the bottom lists
+      const span = this.createElement('span')
+      span.append(this.h4finish, this.finishList, this.h4remove, this.removeList)
+      this.bottomLists = span
+      this.bottomLists.classList.add('bottom-list')
+      
+
       // append title, task-list, form, finish-list and remove-list to the app
-      this.app.append(this.title, this.taskList, this.form, this.finishList, this.removeList)
+      this.app.append(this.h1, this.taskList, this.form, this.bottomLists)
       
       
       this._temporaryTaskText = ''
@@ -240,11 +276,29 @@
         list.forEach(task => {
           const li = this.createElement('li')
           li.id = task.id
-          if (task.status == ModelTask.statusNone) {
-          const checkbox = this.createElement('input')
-          checkbox.type = 'checkbox'
-          checkbox.checked = Model.statusFinish
-          li.append(checkbox)
+          // if the task has no status create a checkbox and remove-button 
+          if (task.status === ModelTask.statusNone) {
+            
+            // create checkbox for finishing tasks
+            const checkbox = this.createElement('input')
+            checkbox.type = 'checkbox'
+            checkbox.checked = Model.statusFinish
+
+            // create remove-button for removing tasks
+            const removeButton = this.createElement('button', 'remove')
+            removeButton.textContent = 'Remove'
+
+            //append the checkbox and remove-button to the list item
+            li.append(checkbox, removeButton)
+          }
+
+          if (task.status === ModelTask.statusFinish || task.status === ModelTask.statusRemove) {
+            // create recover-button for recovering tasks
+            const recoverButton = this.createElement('button', 'recover')
+            recoverButton.textContent = 'Recover'
+
+            //append the recover-button to the list item
+            li.append(recoverButton)
           }
   
           const span = this.createElement('span')
@@ -262,13 +316,12 @@
           const tommorowButton = this.createElement('button', 'tommorow')
           tommorowButton.textContent = 'Tommorow'
 
-          const removeButton = this.createElement('button', 'remove')
-          removeButton.textContent = 'Remove'
-          li.append( span, removeButton)
+          
+          li.append( span)
   
           // Append nodes
           // Append to tasklist
-          if (task.status == ModelTask.statusNone) {
+          if (task.status === ModelTask.statusNone) {
           this.taskList.append(li)
           }
           // Append to finishlist
@@ -283,7 +336,7 @@
       }
   
       // Debugging
-      console.log(list)
+      console.table(list)
     }
   
     _initLocalListeners() {
@@ -314,7 +367,7 @@
         }
       })
     }
-  
+
     bindEditTask(handler) {
       this.taskList.addEventListener('focusout', event => {
         if (this._temporaryTaskText) {
@@ -329,6 +382,23 @@
     bindFinishTask(handler) {
       this.taskList.addEventListener('change', event => {
         if (event.target.type === 'checkbox') {
+          const id = parseInt(event.target.parentElement.id)
+  
+          handler(id)
+        }
+      })
+    }
+
+    bindRecoverTask(handler) {
+      this.finishList.addEventListener('click', event => {
+        if (event.target.className === 'recover') {
+          const id = parseInt(event.target.parentElement.id)
+  
+          handler(id)
+        }
+      })
+      this.removeList.addEventListener('click', event => {
+        if (event.target.className === 'recover') {
           const id = parseInt(event.target.parentElement.id)
   
           handler(id)
@@ -357,6 +427,7 @@
       this.view.bindAddTask(this.storeAddTask)
       this.view.bindEditTask(this.storeEditTask)
       this.view.bindRemoveTask(this.storeRemoveTask)
+      this.view.bindRecoverTask(this.storeRecoverTask)
       this.view.bindFinishTask(this.storeFinishTask)
       // this.view.bindScheduleTodayTask(this.storeScheduleTodayTask)
 
@@ -374,6 +445,9 @@
     }
     storeRemoveTask = id => {
       this.model.removeTask(id)
+    }
+    storeRecoverTask = id => {
+      this.model.recoverTask(id)
     }
     storeFinishTask = id => {
       this.model.finishTask(id)
