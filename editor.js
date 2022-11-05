9d4040618a586430df9b68e8d518c9d566c212da
bubliman <ogabrhelik@ahojky.com>
2022-11-05 23:48:30 +0100

    classes & based changes

--- editor.js
+++ editor.js
@@ -1,49 +1,84 @@
+/* Load in lists from Localstorage */
+let list = JSON.parse(localStorage.getItem('list')) ?? { }
+if (list = undefined) {
+  let list = new List ('Untitled List', , )
+}
+renderList()  
+  
+class List {
+  constructor(title) {
+    this.id = Date.now()
+    this.title = title
+    this.tasksMap = new Map();
+    tasksMap.set(new Task ('Click here to add a task', this.id))
+  }
+  updateList(tasks) {
+    this.id === id ? this.tasks = tasks : this.status
+  }
+  addTaskToList() {
+    new Task
+  }
+}
 /* Load in tasks from Localstorage */
-let tasks = JSON.parse(localStorage.getItem('tasks')) ?? { }
-renderTasks()
+// let tasks = JSON.parse(localStorage.getItem('tasks')) ?? { }
+// renderTasks()
 
 function addTask() {
-    if(document.getElementById('task-input').value != '') {
-        setTask(Date.now(), document.getElementById('task-input').value, 'active')
-        document.getElementById('task-input').value = ''
-        document.getElementById('task-input').select()
+    let taskInput = document.getElementById('task-input')
+    if(taskInput.value != '') {
+      addTaskToList()
+      taskInput.value = ''
+      taskInput.select()
+      renderList()
     }
-}
+  }
 
 /*
  * status: active | removed | finished
 */
-function setTask(id, title, status) {
-    tasks[id] = {
-        "title": title,
-        "status": status,
-    }
-    renderTasks()
-    pushChanges()
+class Task {
+  constructor(title, listID) {
+    this.id = Date.now()
+    this.listID = listID
+    this.title = title
+    this.status = 'active'
+    
+  }
+  static listID = this.listID
+  finishTask(id) {
+    this.id === id ? this.status = 'finished' : this.status
+  }
+  activateTask() {
+    this.id === id ? this.status = 'active' : this.status
+  }
+  removeTask() {
+    this.id === id ? this.status = 'removed' : this.status
+  }
 }
 
-function renderTasks() {
-    const list = document.getElementById('task-list')
-    list.innerHTML = ''
+function renderList() {
+    const domListTitle = document.getElementById('list-title')
+    const domList = document.getElementById('tasklist')
+    domList.innerHTML = ''
 
     for(const id in tasks)
     {
         if(tasks[id].status != 'removed')
-            list.innerHTML += createHTML(tasks[id], id)
+        domList.innerHTML += createHTML(tasks[id], id)
     }
 
     /* Also update the JSON if SPAN is changed, eh hacky */
-    document.querySelectorAll('[class~=task-title]').forEach(element => {
-        element.addEventListener("input", function(e) {
-            tasks[e.srcElement.parentElement.id].title = e.explicitOriginalTarget.textContent
-            pushChanges()
-        }, false);
-    });
+    // document.querySelectorAll('[class~=task-title]').forEach(element => {
+    //     element.addEventlistener("input", function(e) {
+    //         tasks[e.srcElement.parentElement.id].title = e.explicitOriginalTarget.textContent
+    //         pushChanges()
+    //     }, false);
+    // });
 }
 
 /* Save all tasks to localStorage */
 function pushChanges() {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('lists', JSON.stringify(list));
 }
 
 /* Template for a task */
@@ -53,11 +88,11 @@ function createHTML(task, id) {
             <span class="task-title ${task.status == 'finished' ? 'finished' : ''} editable" contenteditable="true">${task.title}</span>
 
             ${task.status == 'finished' ? `
-            <button onclick="setTask(${id}, '${task.title}', 'active')" class="recover">Recover</button>
-            <button onclick="setTask(${id}, '${task.title}', 'removed')" class="remove">Remove</button>
+            <button onclick="activateTask(${id})" class="activate">Activate</button>
+            <button onclick="removeTask(${id})" class="remove">Remove</button>
             ` : `
-            <button onclick="setTask(${id}, '${task.title}', 'finished')" class="finish">Finish</button>
-            <button onclick="setTask(${id}, '${task.title}', 'removed')" class="remove">Remove</button>
+            <button onclick="finishTask(${id})" class="finish">Finish</button>
+            <button onclick="removeTask(${id})" class="remove">Remove</button>
             `
         }
         </li>`
