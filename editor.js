ccbfb0cd3d153c392b3e236c95b0f303eab121ea
bubliman <ogabrhelik@ahojky.com>
2022-11-07 14:46:54 +0100

    title now editable with proper ui

--- editor.js
+++ editor.js
@@ -1,46 +1,53 @@
-
 class List {
-  constructor(title) {
+  constructor() {
     this.id = Date.now()
-    this.title = title
+    this.title = 'Untitled List'
     this.tasksMap = new Map();
     const id = Date.now()
-    this.tasksMap.set(id, new Task (id, 'Click here to add a task', this.id))
+    // this.tasksMap.set(id, new Task (id, 'Click here to add a task', this.id))
   }
   addTaskToList(title) {
     let taskID = Date.now()
     this.tasksMap.set(taskID, new Task (taskID, title, this.id))
     console.log(this.tasksMap)
-    
   }
   renderList() {
     const domListTitle = document.getElementById('list-title')
-    domListTitle.innerText = this.title
+    domListTitle.innerHTML = ''
+    let domListTitleInput = document.createElement('input')
+    domListTitleInput.classList = 'title-input'
+    if (this.title === 'Untitled List') {
+      domListTitleInput.classList.add('untitled')
+      domListTitleInput.addEventListener('focus', event => {
+        domListTitleInput.select()
+      })
+    }
+    domListTitleInput.addEventListener('focusout', event => {
+      domListTitleInput.value = list.editTitle(domListTitleInput.value)
+    })
+    domListTitleInput.addEventListener('keypress', event => {
+      if (event.key === 'Enter') {
+      domListTitleInput.value = list.editTitle(domListTitleInput.value)
+      }
+    })
+    domListTitleInput.value = this.title
+    domListTitle.append(domListTitleInput)
     const domList = document.getElementById('tasklist')
     domList.innerHTML = ''
 
     this.tasksMap.forEach(task => {
       if(task.status != 'removed')
       domList.innerHTML += createHTML(task)
-      this.bindButtons()
     })
-    
-    
-    
-    // domList.addEventListener('click', event => {
-    //   if (event.target.className === 'remove') {
-    //     const id = parseInt(event.target.parentElement.id)
-    //     let task = this.tasksMap.get(id)
-    //     task.removeTask(id)
-    //   }
-    // })
-    // domList.addEventListener('click', event => {
-    //   if (event.target.className === 'finish') {
-    //     const id = parseInt(event.target.parentElement.id)
-    //     let task = this.tasksMap.get(id)
-    //     task.finishTask(id)
-    //   }
-    // })
+    domList.addEventListener('focusout', event => {
+      let task = event.parentElement
+      // task.editTask(task.value)
+    })
+    domList.addEventListener('keypress', event => {
+      if (event.key === 'Enter') {
+      domListTitleInput.value = list.editTitle(domListTitleInput.value)
+      }
+    })
 
     /* Also update the JSON if SPAN is changed, eh hacky */
     // document.querySelectorAll('[class~=task-title]').forEach(element => {
@@ -50,6 +57,11 @@ class List {
     //     }, false);
     // });
   }
+  editTitle(title) {
+    this.title = title
+    this.renderList()
+  }
+  listTitle
 
 }
 
@@ -64,19 +76,23 @@ class Task {
   static listID = this.listID
   finishTask(id) {
     this.status = 'finished'
+    list.renderList()
   }
   activateTask(id) {
     this.id === id ? this.status = 'active' : this.status
+    list.renderList()
   }
   removeTask(id) {
     this.id === id ? this.status = 'removed' : this.status
+    list.renderList()
   }
+  editTask() {}
 }
 
 // /* Load in lists from Localstorage */
 // let list = JSON.parse(localStorage.getItem('list')) ?? { }
 // if (list) {
-  let list = new List ('Untitled List')
+  let list = new List()
 // }
 list.renderList()  
   
@@ -89,36 +105,31 @@ function addTask() {
       list.renderList()
     }
   }
-  
+
   const domList = document.getElementById('tasklist')
+  const domListTitle = document.getElementById('list-title')
+  const domListTitleInput = domListTitle.firstChild
   domList.addEventListener('click', event => {
     if (event.target.className === 'activate') {
       const id = parseInt(event.target.parentElement.id)
       let task = list.tasksMap.get(id)
       task.activateTask(id)
-      list.renderList
     }
     if (event.target.className === 'remove') {
       const id = parseInt(event.target.parentElement.id)
       let task = list.tasksMap.get(id)
       task.removeTask(id)
-      list.renderList
+      
     }
     if (event.target.className === 'finish') {
       const id = parseInt(event.target.parentElement.id)
       let task = list.tasksMap.get(id)
       task.finishTask(id)
-      list.renderList
     }
-    
-  })
-
   
-
-/*
- * status: active | removed | finished
-*/
-
+  })
+ 
+  
 
 /* Save all tasks to localStorage */
 function pushChanges() {
@@ -135,14 +146,14 @@ function createHTML(task) {
   let id = task.id
     return `
         <li id="${id}">
-            <span class="task-title ${task.status == 'finished' ? 'finished' : ''} editable" contenteditable="true">${task.title}</span>
+            <input class="task-title ${task.status == 'finished' ? 'finished' : ''}" value="${task.title}">
 
             ${task.status == 'finished' ? `
-            <button class="activate">Activate</button>
-            <button class="remove">Remove</button>
+            <button class="activate">✘</button>
+            <button class="remove">🗑️</button>
             ` : `
-            <button class="finish">Finish</button>
-            <button class="remove">Remove</button>
+            <button class="finish">✔</button>
+            <button class="remove">🗑️</button>
             `
         }
         </li>`
